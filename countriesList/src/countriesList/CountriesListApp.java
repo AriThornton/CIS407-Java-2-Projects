5e604809f1ad2b3182f4a93dee9264d1644a7aad
Ari Thornton <73207741+AriThornton@users.noreply.github.com>
2024-06-16 15:43:16 -0700

    Add files via upload

---
+++ countriesList/src/countriesList/CountriesListApp.java
@@ -0,0 +1,25 @@
+package countriesList;
+import java.util.Scanner;
+public class CountriesListApp {
+	public static void main(String[] args) {
+		try (Scanner userInput = new Scanner(System.in)){
+			CountriesList countriesList = new CountriesList();
+			countriesList.displayWelcomeMessage();
+			countriesList.displayMenu();
+			main: while (true) {
+				countriesList.getMenuOption(userInput);
+				switch(countriesList.menuOption) {
+				case LIST:
+					countriesList.listCountries();
+					break;
+				case ADD:
+					countriesList.addCountry(userInput);
+					break;
+				case END:
+					break main;
+				}
+			}
+			System.out.println("Goodbye!");
+		}
+	}
+}
