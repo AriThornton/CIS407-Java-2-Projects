10341deba248deca452fd744cd1b28f23634ffbf
Ari Thornton <73207741+AriThornton@users.noreply.github.com>
2024-06-16 15:45:20 -0400

    Update DataEntry.java

--- courseProjectPhase1/DataEntry.java
+++ courseProjectPhase1/DataEntry.java
@@ -1,93 +1,24 @@
 package courseProject;
-import java.util.ArrayList;
-import java.util.List;
-public class Customer {
-	String customerID;
-	String SSN;
-	String lastName;
-	String firstName;
-	String streetName;
-	String cityName;
-	String state;
-	String zipCode;
-	String phoneNumber;
-	public void setCustomerID(String id) {
-		this.customerID = id;
+public class DataEntry {
+	public static boolean validateString(String entry) {
+		return (!(entry.isBlank()));
 	}
-	public String getCustomerID() {
-		return this.customerID;
+	public static boolean validateStringLength(String entry, int length) {
+		return (!(entry.isBlank()) && (entry.length() <= length));
 	}
-	public void setSSN(String ssn) {
-		this.SSN = ssn;
+	public static boolean validateStringExactLength(String entry, int length) {
+		return (entry.length() == length);
 	}
-	public String getSSN() {
-		return this.SSN;
+	public static boolean validateStringNumeric(String entry) {
+		return entry.matches("\\d+");
 	}
-	public void setLastName(String name) {
-		this.lastName = name;
+	public static boolean validateIntRange(int entry, int min, int max) {
+		return (min <= entry && entry <= max);
 	}
-	public String getLastName() {
-		return this.lastName;
+	public static boolean validateDecimalRange(float entry, float min, float max) {
+		return (min <= entry && entry <= max);
 	}
-	public void setFirstName(String name) {
-		this.firstName = name;
-	}
-	public String getFirstName() {
-		return this.firstName;
-	}
-	public void setStreet(String street) {
-		this.streetName = street;
-	}
-	public String getStreet() {
-		return this.streetName;
-	}
-	public void setCity(String city) {
-		this.cityName = city;
-	}
-	public String getCity() {
-		return this.cityName;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getState() {
-		return this.state;
-	}
-	public void setZip(String zip) {
-		this.zipCode = zip;
-	}
-	public String getZip() {
-		return this.zipCode;
-	}
-	public void setPhone(String phone) {
-		this.phoneNumber = phone;
-	}
-	public String getPhone() {
-		return this.phoneNumber;
-	}
-	public List<String> toStringList() {
-		List<String> result = new ArrayList<>();
-		result.add(this.customerID);
-		result.add(this.SSN);
-		result.add(this.lastName);
-		result.add(this.firstName);
-		result.add(this.streetName);
-		result.add(this.cityName);
-		result.add(this.state);
-		result.add(this.zipCode);
-		result.add(this.phoneNumber);
-		return result;
-	}
-	public String toString() {
-		String result = this.customerID + "\t";
-		result += this.SSN + "\t";
-		result += this.lastName + "\t";
-		result += this.firstName + "\t";
-		result += this.streetName + "\t";
-		result += this.cityName + "\t";
-		result += this.state + "\t";
-		result += this.zipCode + "\t";
-		result += this.phoneNumber;
-		return result;
+	public static boolean validateDate(String entry) {
+		return (entry.matches("\\d{1,2}[/]\\d{1,2}[/]\\d{4}"));
 	}
 }
